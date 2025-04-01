8da8cd98ed1bbacc2ba946e114b80ff39edc953a
vicuartas230 <vicuartas230@gmail.com>
2025-03-31 20:21:09 -0500

    Subir entidades

---
+++ src/main/java/com/egg/libreriaapi/entidades/Autor.java
@@ -0,0 +1,46 @@
+package com.egg.libreriaapi.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private boolean activo;
+
+    public Autor() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
---
+++ src/main/java/com/egg/libreriaapi/entidades/Libro.java
@@ -0,0 +1,84 @@
+package com.egg.libreriaapi.entidades;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Libro {
+    @Id
+    private Long isbn;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    private Integer ejemplares = 1;
+
+    @Temporal(TemporalType.DATE)
+    private Date alta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_editorial")
+    private Editorial editorial;
+
+    public Libro() {
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getEjemplares() {
+        return ejemplares;
+    }
+
+    public void setEjemplares(Integer ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public Date getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Date alta) {
+        this.alta = alta;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+}
