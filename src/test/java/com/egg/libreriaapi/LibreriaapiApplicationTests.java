10c9009f77ac3bd0973187f1570d8c42862f69a1
vicuartas230 <vicuartas230@gmail.com>
2025-03-31 20:17:22 -0500

    Commit inicial

---
+++ src/test/java/com/egg/libreriaapi/LibreriaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.libreriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
